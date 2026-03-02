5beb31c73210f321fc160e6aa19dcffc1ae80cbe
Xanderdxp <xerozion@gmail.com>
2026-03-02 20:11:28 +0000

    Final

--- Main.java
+++ Main.java
@@ -1,5 +1,5 @@
-// STUDENT NAME:
-// DATE: 
+// STUDENT NAME: Xander
+// DATE: 3/1/26
 
 public class Main 
 {
@@ -7,15 +7,27 @@ public class Main
 	{
 
 		/***** DECLARATION SECTION *****/
+		int y;
+		int m;
+		int a;
+		int JDN;
 
 		/***** INITIALIZATION SECTION *****/
+		int year = 2020;
+		int month = 2;
+		int day = 2;
 		
 		/***** INPUT SECTION *****/
 		// N/A (no input for this lab)
-		
-		/***** CALCULATION & PROCESSING SECTION *****/
 
 		
+		/***** CALCULATION & PROCESSING SECTION *****/
+		a = (14 - month) / 12;
+		m = month + (12 * a) -3;
+		y = year + 4800 - a;
+		JDN = day + (153 * m +2) / 5 + 365 * y + (y / 4) - (y/100) + (y/400) - 32045;
+		
 		/***** OUTPUT SECTION *****/
+		System.out.println("Julian Day Number:" + JDN);
   }
 }
